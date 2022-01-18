f74f416e7aa8f17a192dfcfd732bb4d19379fc2f
franciscodr <francisco.d@47deg.com>
2022-01-18 18:33:25 +0100

    Add test cases

---
+++ src/test/scala/io/example/SubscriptionServiceTests.scala
@@ -0,0 +1,67 @@
+package io.example
+
+import cats.data.StateT
+import cats.effect.IO
+import io.example.interpreters._
+import io.example.model.{Repository, SubscriptionAction, User}
+
+class SubscriptionServiceTests extends munit.CatsEffectSuite {
+  val subscriptionService = new SubscriptionService[StateT[IO, ServiceState, *]](
+    new SubscriptionPersistenceTestImpl(),
+    new UserPersistenceTestImpl(),
+    new MessageProducerTestImpl()
+  )
+
+  test("The service only sends a message to the queue for the first subscription to a repo") {
+    val slackIdForUserA = User.SlackId("abc")
+    val slackIdForUserB = User.SlackId("def")
+    val repository      = Repository(Repository.Organization("47deg"), Repository.Name("test"))
+
+    val subscriptionForUserA = subscriptionService.subscribeRepository(slackIdForUserA, repository)
+    val subscriptionForUserB = subscriptionService.subscribeRepository(slackIdForUserB, repository)
+
+    val serviceStates = for {
+      stateAfterSubscriptionForUserA <- subscriptionForUserA.runS(ServiceState.empty)
+      stateAfterSubscriptionForUserB <- subscriptionForUserB.runS(stateAfterSubscriptionForUserA)
+    } yield (stateAfterSubscriptionForUserA, stateAfterSubscriptionForUserB)
+
+    serviceStates.map { case (stateAfterSubscriptionForUserA, stateAfterSubscriptionForUserB) =>
+      stateAfterSubscriptionForUserA.subscriptionKafkaTopic.length == 1 &&
+      stateAfterSubscriptionForUserA.subscriptionKafkaTopic.exists(message =>
+        message.repository == repository && message.action == SubscriptionAction.Subscribe
+      ) &&
+      stateAfterSubscriptionForUserB.subscriptionKafkaTopic.length == 1
+    }.assert
+  }
+
+  test(
+    "After canceling a subscription, if there are no active subscriptions for the repo, the service sends one message to the queue."
+  ) {
+    val slackIdForUserA = User.SlackId("abc")
+    val slackIdForUserB = User.SlackId("def")
+    val repository      = Repository(Repository.Organization("47deg"), Repository.Name("test"))
+
+    val subscriptionCancellationForUserA =
+      subscriptionService.unsubscribeRepository(slackIdForUserA, repository)
+    val subscriptionCancellationForUserB =
+      subscriptionService.unsubscribeRepository(slackIdForUserB, repository)
+
+    val serviceStates = for {
+      stateAfterCancellingSubscriptionForUserA <- subscriptionCancellationForUserA.runS(
+        ServiceState.generateSubscriptions(repository, List(slackIdForUserA, slackIdForUserB))
+      )
+      stateAfterCancellingSubscriptionForUserB <- subscriptionCancellationForUserB.runS(
+        stateAfterCancellingSubscriptionForUserA
+      )
+    } yield (stateAfterCancellingSubscriptionForUserA, stateAfterCancellingSubscriptionForUserB)
+
+    serviceStates.map {
+      case (stateAfterCancellingSubscriptionForUserA, stateAfterCancellingSubscriptionForUserB) =>
+        stateAfterCancellingSubscriptionForUserA.subscriptionKafkaTopic.isEmpty &&
+        stateAfterCancellingSubscriptionForUserB.subscriptionKafkaTopic.length == 1 &&
+        stateAfterCancellingSubscriptionForUserB.subscriptionKafkaTopic.exists(message =>
+          message.repository == repository && message.action == SubscriptionAction.Unsubscribe
+        )
+    }.assert
+  }
+}
